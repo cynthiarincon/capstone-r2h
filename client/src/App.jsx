5a1ab3b10587aa09f19d709dcc037be945cfb3e3
Cynthia Rincon <cynthiarincon@MacBookPro.lan>
2026-03-18 18:24:09 -0400

    Feat: Set up App.jsx with React Router, Navbar, and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,19 @@
-//placeholder text
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import './index.css'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
